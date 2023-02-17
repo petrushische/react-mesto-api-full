31c7202ed2d69405f8990bb6fe2f9e449a5ef61e
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-02-17 15:44:22 +0300

    feat backend CORS

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,30 @@ const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process
 
 const app = express();
 
+const allowedCors = [
+  'https://petrosellinum.nomoredomains.work',
+  'http://petrosellinum.nomoredomains.work',
+  'localhost:3000',
+];
+
 app.use(requestLogger); //  логгер запросов
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
 app.post('/signin', express.json(), celebrate({
   body: Joi.object().keys({
