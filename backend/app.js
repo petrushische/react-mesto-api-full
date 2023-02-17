404976dda9a86afe1cbeba1eacedaec9b6e60a6b
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-02-18 00:56:26 +0300

    fix app.js backend

--- backend/app.js
+++ backend/app.js
@@ -34,16 +34,15 @@ const allowedCors = [
 // eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
   const { method } = req;
+  const { origin } = req.headers;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  } if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
-  }
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
   }
   next();
 });
