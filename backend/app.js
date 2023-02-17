bd3a2edae1a21e37a751d7f6028c53cc3e084812
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-02-17 19:32:01 +0300

    fix new db mongo

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { login, createUser } = require('./controllers/user');
 
-const { PORT = 3000, MONGO_URL = ' mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
